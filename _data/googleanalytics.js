362bba9c688d2a790dd1d83fbd9b17cbe38fabd6
Precious13ui <pitmandonna5@gmail.com>
2022-12-10 11:48:42 +0000

    New post

--- _data/googleanalytics.js
+++ _data/googleanalytics.js
@@ -23,7 +23,7 @@ const metadata = require("./metadata.json");
 
 module.exports = function () {
   let id = metadata.googleAnalyticsId;
-  if (/Update me/.test(id)) {
+  if (/precious13ui.github.io/.test(id)) {
     id = null;
   }
   return id;
